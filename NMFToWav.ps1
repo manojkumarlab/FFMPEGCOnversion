2f6cdb1ea455ac82028bc19cdc01634ad213e4cf
manojkumarlab <78903612+manojkumarlab@users.noreply.github.com>
2021-04-10 12:50:38 +0530

    Update NMFToWav.ps1

--- NMFToWav.ps1
+++ NMFToWav.ps1
@@ -5,7 +5,7 @@ $oldVideos = Get-ChildItem -Include @("*.nmf") -Path $inProcessPath -Recurse;
 Set-Location -Path 'E:\FFMPEG\bin\';
 
 foreach ($oldVideo in $oldVideos) {
-    $newVideo = [io.path]::ChangeExtension($oldSong.FullName, '.wav')
+    $newVideo = [io.path]::ChangeExtension($oldVideo.FullName, '.wav')
 
     $ArgumentList = '-i "{0}" -y -async 1 -b 2000k -ar 44100 -ac 2 -v 0 -f flv -vcodec libx264 -preset superfast "{1}"' -f $oldVideo, $newVideo;
 
@@ -13,4 +13,8 @@ foreach ($oldVideo in $oldVideos) {
     $null = Read-Host -Prompt 'Press enter to continue, after verifying command line arguments.';
 
     Start-Process -FilePath c:\path\to\ffmpeg.exe -ArgumentList $ArgumentList -Wait -NoNewWindow;
+}
+
+Get-ChildItem .\video_old -Filter *.mkv | ForEach-Object {
+  ffmpeg -i $_.FullName -c:v libx265 -x265-params lossless=1 ".\video_new\$($_.Name)"
 }
